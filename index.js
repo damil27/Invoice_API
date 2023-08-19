1763c70a74e01adc9baf0d777ac4e13083bff4f8
damin <34189426+damil27@users.noreply.github.com>
2023-08-19 10:29:51 +0100

    post and delete functionality completed

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
-let data = require("./data");
-app.use(express.json())
+const mongoose = require("mongoose");
+const invoiceRouter = require("./routes/InvoiceRouter");
+const cors = require("cors")
 
 
-console.log(data);
-app.get("/api/invoice", (request, response) => {
-  response.send(data);
-});
-
-app.get("/api/invoice/:id", (request, response) => {
-  //   response.send(request.params)
-  const invoice = data.find((item) => item.id === request.params.id);
-  if (!invoice) response.status(404).send("resource not found");
-
-  response.send(invoice);
-});
-
-app.post("api/invoice", (request, response) => {
-    const newInvoice = request.body;
-    newInvoice.id = "WW20"
-    data.push(newInvoice)
-    response.status(201).send(data)
-})
-
-app.put("api/invoice/:id ", (request, response) => { 
-    const updatedDataId = data.findIndex(item => item.id === request.params.id)
-    if (!updatedData) response.status(404).send(`Invoice with ${request.params.id} can not be found`)
-    const updatedDatad = request.body
-    data[updatedDataId] = {...data[updatedDataId],  ...updatedDatad}
-    response.send(data[updatedDataId]);
-})
+app.use(express.json());
+app.use(cors());
 
 
+// console.log(data);
+if (app.get(`env`) === `development`) {
+  app.use(morgan("tiny"));
+}
+
+app.use("/api/invoice", invoiceRouter);
+
+mongoose
+  .connect(
+    "mongodb+srv://dhamilare27:bWprmkOonjfiRavf@cluster0.tapj5de.mongodb.net/invoices?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connnected to database...");
+    // createInvoice();
+  })
+  .catch((err) => console.error("could not connect to database", err));
 const port = process.env.PORT || 3088;
 app.listen(port, () => console.log(`Listening on port ${port}`));
